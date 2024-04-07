e9a79543a947e2e3bb7aa2d75343303c049ca2f1
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-04-07 07:33:36 -0700

    web_static setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+# Remove existing symbolic link if it exists
+sudo rm -rf /data/web_static/current
+# Create new symbolic link
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
